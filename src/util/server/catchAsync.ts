4659f42edba0ba4d75e544f900c69fd62bed6b24
shaishabcoding <shaishabchandrashil@gmail.com>
2025-04-24 22:58:40 +0600

    fix

--- src/util/server/catchAsync.ts
+++ src/util/server/catchAsync.ts
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-expressions */
 import { RequestHandler } from 'express';
-import { Types } from 'mongoose';
 
 /**
  * Wraps an Express request handler to catch and handle async errors
@@ -9,14 +8,7 @@ import { Types } from 'mongoose';
  * @returns A wrapped request handler that catches async errors
  */
 const catchAsync =
-  (
-    fn: RequestHandler<
-      { [key: string]: Types.ObjectId },
-      any,
-      any,
-      { [key: string]: number | string }
-    >,
-  ): RequestHandler =>
+  (fn: RequestHandler<any, any, any, any>): RequestHandler =>
   async (req, res, next) => {
     try {
       await fn(req as any, res, next);
