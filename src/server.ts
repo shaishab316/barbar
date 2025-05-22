2232d225100645e502ba9b4b17ed7b7ae58e1170
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-22 22:54:21 +0600

    feat: Implement basic Socket.IO server for messaging
    This commit initializes a Socket.IO server within the existing server setup.  It handles user connections and disconnections, and implements a `sendMessage` event to create and broadcast messages within chat rooms. It also triggers inbox updates for users involved in the chat.  CORS configuration is applied using `config.server.allowed_origins`.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,48 @@
+/* eslint-disable no-console */
+import { Server } from 'socket.io';
 import './util/prototype'; //! must be first
 import startServer from './util/server/startServer';
-import { SocketService } from './app/modules/socket/Socket.service';
+import config from './config';
+import Message from './app/modules/message/Message.model';
+import Chat from './app/modules/chat/Chat.model';
+
+export let io: Server | null;
 
 startServer().then(server => {
-  /** Micro services */
-  SocketService.init(server);
+  if (!io)
+    io = new Server(server, {
+      cors: { origin: config.server.allowed_origins },
+    });
+
+  io.on('connection', socket => {
+    console.log('👤 User connected');
+
+    socket.on('disconnect', () => {
+      console.log('👤 User disconnected');
+    });
+
+    socket.on('error', err => {
+      console.log(err);
+      socket.disconnect();
+    });
+
+    socket.on('sendMessage', async ({ roomId, sender, message }: any) => {
+      try {
+        const newMessage = await Message.create({
+          chat: roomId,
+          sender,
+          content: message,
+        });
+        io?.emit(`messageReceived:${roomId}`, newMessage);
+        console.log(newMessage);
+        const chat = await Chat.findById(roomId);
+
+        chat?.users.forEach(user => {
+          io?.emit(`inboxUpdated:${user}`, newMessage);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
 });
